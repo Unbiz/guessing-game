5b82b88ac663ef3149773afa0fd8a4d9d384bec2
Vladimir Bessonov <bessmy@gmail.com>
2021-08-28 10:23:45 +0300

    feat: add GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.first = min;
+        this.last = max;
+        this.middle;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.first + this.last) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.last = this.middle;
     }
 
     greater() {
-
+        this.first = this.middle;
     }
 }
 
